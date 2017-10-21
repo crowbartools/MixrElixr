14e11f25f609f6c6ebd5bb9a6a85a8fdb25cbbc7
Erik Bigler <erikbigler@gmail.com>
2017-10-21 00:16:47 -0700

    add light version of icon

--- options/options.js
+++ options/options.js
@@ -2,10 +2,10 @@
 	VUE COMPONENTS
 */
 Vue.component('nav-bar', {
-	template: `
+    template: `
 		<nav class="navbar navbar-expand navbar-dark bg-dark">
 			<a class="navbar-brand">
-				<img src="/resources/images/elixr-128.png" width="30" height="30" alt="">
+				<img src="/resources/images/elixr-light-128.png" width="30" height="30" alt="">
 			</a>
 			<div class="collapse navbar-collapse" id="navbarNav">
 				<ul class="navbar-nav">
@@ -19,25 +19,25 @@ Vue.component('nav-bar', {
 			</div>
 		</nav>
 	`,
-	data: function() {
-		return {
-			activeTab: 'online',
-			onlineActive: true,
-			optionsActive: false
-		}  
-	},
-	props: ['onlineCount'],
-	methods: {
-		changeTab: function(tab) {
-			this.onlineActive = tab === 'online';
-			this.optionsActive = tab === 'options';
-			this.$emit('tab-changed', tab)
-		}
-	}
-})
+    data: function() {
+        return {
+            activeTab: 'online',
+            onlineActive: true,
+            optionsActive: false
+        };
+    },
+    props: ['onlineCount'],
+    methods: {
+        changeTab: function(tab) {
+            this.onlineActive = tab === 'online';
+            this.optionsActive = tab === 'options';
+            this.$emit('tab-changed', tab);
+        }
+    }
+});
 
 Vue.component('checkbox-toggle', {
-	template: `
+    template: `
 		<label style="display:flex">
 			<label class="switch">
 				<input type="checkbox" v-model="value" @change="valueUpdated"/>
@@ -46,14 +46,14 @@ Vue.component('checkbox-toggle', {
 			{{label}}
 		</label>
 	`,
-	props: ['value', "label"],
-	methods: {
-		valueUpdated: function() {
-			this.$emit('update:value', this.value)
-			this.$emit('change');
-		}
-	}
-})
+    props: ['value', 'label'],
+    methods: {
+        valueUpdated: function() {
+            this.$emit('update:value', this.value);
+            this.$emit('change');
+        }
+    }
+});
 
 
 
@@ -63,85 +63,85 @@ Vue.component('checkbox-toggle', {
 */
 
 var settingsStorage = {
-	fetch: function(defaultSettings) {
-		return new Promise((resolve, reject) => {
-			chrome.storage.sync.get({
-				"settings": defaultSettings
-			}, function(data) {
-				resolve(data);
-			});
-		});
-	}, 
-	save: function(settings) {
-		chrome.storage.sync.set({ "settings": settings }, () => {		
-			settingsStorage.emitEvent();
-		});
-	},
-	emitEvent: function() {
-		// let the content script on whatever tab know the settings have been updated
-		chrome.tabs.query({}, function(tabs) {
-			var message = { settingsUpdated: true };
-			for (var i=0; i<tabs.length; ++i) {
-				chrome.tabs.sendMessage(tabs[i].id, message);
-			}
-		});
-	}
-}
+    fetch: function(defaultSettings) {
+        return new Promise((resolve) => {
+            chrome.storage.sync.get({
+                'settings': defaultSettings
+            }, function(data) {
+                resolve(data);
+            });
+        });
+    },
+    save: function(settings) {
+        chrome.storage.sync.set({ 'settings': settings }, () => {
+            settingsStorage.emitEvent();
+        });
+    },
+    emitEvent: function() {
+        // let the content script on whatever tab know the settings have been updated
+        chrome.tabs.query({}, function(tabs) {
+            var message = { settingsUpdated: true };
+            for (var i = 0; i < tabs.length; ++i) {
+                chrome.tabs.sendMessage(tabs[i].id, message);
+            }
+        });
+    }
+};
 
 var app = new Vue({
-	el: '#app',
-	data: {
-		autoCloseInteractive: false,
-		separateChat: false,
-		alternateChatBGColor: false,
-		showImageLinksInline: false,
-		autoForwardOnHost: false,
-		removeHomepageFeatured: false,
-		activeTab: 'online',
-		onlineStreamers: 1
-	},
-	computed: {
-		settings: {
-			get: function() {
-				return {
-					autoCloseInteractive: this.autoCloseInteractive,
-					separateChat: this.separateChat,
-					alternateChatBGColor: this.alternateChatBGColor,
-					showImageLinksInline: this.showImageLinksInline,
-					autoForwardOnHost: this.autoForwardOnHost,
-					removeHomepageFeatured: this.removeHomepageFeatured
-				}
-			}, 
-			set: function(settings) {
-				this.autoCloseInteractive = settings.autoCloseInteractive === true;
-				this.separateChat = settings.separateChat === true;
-				this.alternateChatBGColor = settings.alternateChatBGColor === true;
-				this.showImageLinksInline = settings.showImageLinksInline === true;
-				this.autoForwardOnHost = settings.autoForwardOnHost === true;
-				this.removeHomepageFeatured = settings.removeHomepageFeatured === true;
-			}		
-		}
-	},
-	methods: {
-		fetchSettings: function() {
-			var app = this;
-			settingsStorage.fetch(app.settings).then((data) => {
-				app.settings = data.settings;
-			});
-		},
-		saveSettings: function() {
-			var app = this;
-			settingsStorage.save(app.settings);
-		},
-		updateActiveTab: function(tab) {
-			console.log("tab changed: " + tab);
-			this.activeTab = tab;
-		}
-	},
-	mounted: function() {
-		// When Vue is ready
-		this.fetchSettings();
-	}
+    el: '#app',
+    data: {
+        autoCloseInteractive: false,
+        separateChat: false,
+        alternateChatBGColor: false,
+        showImageLinksInline: false,
+        autoForwardOnHost: false,
+        removeHomepageFeatured: false,
+        activeTab: 'online',
+        onlineStreamers: 1
+    },
+    computed: {
+        settings: {
+            get: function() {
+                return {
+                    autoCloseInteractive: this.autoCloseInteractive,
+                    separateChat: this.separateChat,
+                    alternateChatBGColor: this.alternateChatBGColor,
+                    showImageLinksInline: this.showImageLinksInline,
+                    autoForwardOnHost: this.autoForwardOnHost,
+                    removeHomepageFeatured: this.removeHomepageFeatured
+                };
+            },
+            set: function(settings) {
+                this.autoCloseInteractive = settings.autoCloseInteractive === true;
+                this.separateChat = settings.separateChat === true;
+                this.alternateChatBGColor = settings.alternateChatBGColor === true;
+                this.showImageLinksInline = settings.showImageLinksInline === true;
+                this.autoForwardOnHost = settings.autoForwardOnHost === true;
+                this.removeHomepageFeatured = settings.removeHomepageFeatured === true;
+            }
+        }
+    },
+    methods: {
+        fetchSettings: function() {
+            var app = this;
+            settingsStorage.fetch(app.settings).then((data) => {
+                app.settings = data.settings;
+            });
+        },
+        saveSettings: function() {
+            var app = this;
+            settingsStorage.save(app.settings);
+        },
+        updateActiveTab: function(tab) {
+            console.log('tab changed: ' + tab);
+            this.activeTab = tab;
+        }
+    },
+    mounted: function() {
+        // When Vue is ready
+        this.fetchSettings();
+    }
 });
 
 
