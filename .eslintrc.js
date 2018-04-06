a0f65299af3b66cf267eb035943bc0c42562e8aa
SReject <sreject@comcast.net>
2018-04-06 12:08:23 -0400

    Setup test enviornment

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": 0, // Enable the use of console
+        "indent": [1, "tab"],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "space-before-function-paren": [
+            "error",
+            "never"
+        ],
+        "no-undef": "warn",
+        "no-unused-vars": "warn"
+    },
+    "globals": {
+        "$": false,
+        "chrome": false,
+        "Vue": false
+    }
+};
