7f300ee83601d5ab9715e4c2d4e3cc3dd3a8d92d
Perry <theperry2015@gmail.com>
2018-09-26 10:05:17 +0200

    Changed the text for the toggle
    Changed the text for the toggle and explained the button in a tool tip.
    Removed a line that was left behind after some testing.

--- options/app/components/streamerPageOptions.js
+++ options/app/components/streamerPageOptions.js
@@ -20,8 +20,7 @@ Vue.component('streamer-page-options', {
 				<checkbox-toggle :value.sync="alternateChatBGColor" @changed="saveSettings()" label="Alternate Chat BG Color"></checkbox-toggle>
 				<checkbox-toggle :value.sync="timestampAllMessages" @changed="saveSettings()" label="Timestamp All Messages" tooltip="Please note that timestamps will only be added to new messages as there is no way for us to tell when previous messages were sent."></checkbox-toggle>
 				<checkbox-toggle :value.sync="mentionChatBGColor" @changed="saveSettings()" label="Highlight When Mentioned" tooltip="Apply a special background behind messages when you are mentioned."></checkbox-toggle>
-				<checkbox-toggle :value.sync="hideDeleted" @changed="saveSettings()" label="Change the way deleted messages" tooltip="Whether or not you want to show the deleted messages in another way."></checkbox-toggle>
-				<checkbox-toggle :value.sync="noneStrikedDeleted" @changed="saveSettings()" label="Don't strikeout deleted" tooltip="Whether or not you want to show the deleted messages striked out or just faded."></checkbox-toggle>
+				<checkbox-toggle :value.sync="hideDeleted" @changed="saveSettings()" label="Mask Deleted Messages" tooltip="Applies a blur on the deleted message so that you will have to hover them to read the contents. (Nice for hiding spoilers or bad words)"></checkbox-toggle>
 
 				<div class="option-wrapper">
 					<div style="padding-bottom: 5px;">Highlight Keywords<option-tooltip name="highlightKeywords" title="Any messages containing these keywords will have a special background."></option-tooltip></div>
