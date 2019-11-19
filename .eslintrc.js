e4a6afdc967ba7f5c46eee4bc71dad9d8f6db287
SReject <sreject@comcast.net>
2019-11-18 21:39:38 -0500

    Refactoring linters; prettier and such to come later

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@
 // File taken from https://github.com/vuejs-templates/webpack/blob/1.3.1/template/.eslintrc.js, thanks.
 
 module.exports = {
-    root: true,
     parserOptions: {
         parser: 'babel-eslint'
     },
@@ -18,21 +17,14 @@ module.exports = {
         scriptCommunication: true
     },
     extends: [
-        // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-        // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-        'plugin:vue/essential',
-        // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-        'standard',
-        // https://prettier.io/docs/en/index.html
-        'plugin:prettier/recommended',
-        'prettier'
+        'eslint:recommended',
+        'plugin:vue/essential'
     ],
     // required to lint *.vue files
-    plugins: ['vue', 'prettier'],
+    plugins: ['vue'],
 
     // add your custom rules here
     rules: {
-        "prettier/prettier": "error",
 
         // allow async-await
         "generator-star-spacing": 'off',
@@ -41,13 +33,61 @@ module.exports = {
         "no-debugger": process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
         // allow promises to be rejected without an error message
-        "allowEmptyReject": true,
-        "prefer-promise-reject-errors": "warn",
+        "prefer-promise-reject-errors": ["warn", {"allowEmptyReject": true}],
         
-        // require let and const
-        "no-var": "error",
+        // Deviations from https://eslint.org/docs/rules/#possible-errors
+        "no-console": 0, // Enable the use of console
 
-        // force semi colons
-        "semi": ["error", "always", { "omitLastInOneLineBlock": false}]
+        // Deviations from < https://eslint.org/docs/rules/#best-practices >
+        "eqeqeq": ["error", "smart"],     // No coersion unless comparing against null
+        "guard-for-in": "error",          // require an if statement with for-in loops
+        "no-else-return": "error",        // no 'if () { return } else { ... }
+        "no-eval": "error",               // no eval()
+        "no-floating-decimal": "error",   // no trailing decimals after numbers
+        "no-lone-blocks": "error",        // see:
+        "no-multi-spaces": "error",       // no repeating spaces
+        "no-throw-literal": "error",      // must throw an error instance
+        "no-unused-expressions": "error", // see:
+        "no-with": "error",               // no with statements
+        "wrap-iife": "error",             // immediately called functions must be wrapped in ()'s
+
+        // Deviation from < https://eslint.org/docs/rules/#strict-mode >
+        "strict": "off", // strict not required
+
+        // Deviation from < https://eslint.org/docs/rules/#variables >
+        "no-use-before-define": "error", // require vars to be defined before use
+
+        // Deviation from < https://eslint.org/docs/rules/#stylistic-issues >
+        "array-bracket-spacing": "error",                                     // Spaces around array []'s
+        "block-spacing": "error",                                             // {}'s must have whitespace around them
+        "brace-style": "error",                                               // See: https://eslint.org/docs/rules/brace-style#require-brace-style-brace-style
+        "camelcase": "error",                                                 // useCamelCasePleaseKThanks
+        "comma-dangle": "error",                                              // No trailing commas
+        "comma-spacing": "error",                                             // Reqire space after commas
+        "comma-style": "error",                                               // See: https://eslint.org/docs/rules/comma-style
+        "computed-property-spacing": "error",                                 // No whitespace when using object[thing]
+        "indent": ["error", 4],                                               // Four-space indentions
+        "key-spacing": ["error", {"beforeColon": false, "afterColon": true}], // No space before colons, atleast one space after
+        "keyword-spacing": "error",                                           // Spaces around keywords
+        "linebreak-style": "error",                                           // Line breaks must be \n
+        "new-cap": "error",                                                   // Constructors must start with capital letter
+        "no-trailing-spaces": "error",                                        // no trailing spaces
+        "semi": "error",                                                      // semi-colons required
+        "semi-spacing": ["error", {before: false, after: true}],              // space after semi-colon, no space before
+        "semi-style": "error",                                                // See: https://eslint.org/docs/rules/semi-style
+        "space-before-blocks": "error",                                       // whitespace required before and after {}
+        "space-in-parens": ["error", "never"],                                // See: https://eslint.org/docs/rules/space-in-parens
+        "space-infix-ops": "error",                                           // Spaces required areound operators
+        "space-unary-ops": "error",                                           // See: https://eslint.org/docs/rules/space-unary-ops
+        "switch-colon-spacing": "error",                                      // Spaces after case colon
+
+        // Deviation from < https://eslint.org/docs/rules/#ecmascript-6 >
+        "arrow-spacing": "error",      // Spaces required around fat-arrow function's "=>"
+        "no-confusing-arrow": "error", // Don't use arrows functions in conditions
+        "no-var": "error",             // Warning; Use let/const instead of var
+
+        // Other deviations
+        "no-warning-comments": ["warn", {"terms": ["todo", "to do", "fix", "fixme", "fix me", "need"], "location": "start"}], // warn about todo comments
+        "no-unused-vars": ["error"]
     }
 };
