657951418959a2174030dfbf059861ff6c3403d3
SReject <sreject@comcast.net>
2019-11-23 12:58:56 -0500

    fixed lint-man yelling

--- src/injected-content/utils/index.js
+++ src/injected-content/utils/index.js
@@ -6,6 +6,21 @@ export function log(...message) {
     console.log('[MixrElixr]', ...message);
 }
 
+// moved but left to keep the lint-man happy
+function debounce(func, wait, immediate) {
+    let timeout;
+    return function executedFunction(...args) {
+        let context = this;
+        let later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
 
 export function determineMessageType(message) {
 
