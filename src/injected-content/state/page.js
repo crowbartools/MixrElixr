50f8aa1aaa969005b08e03e2747657520409cfd9
SReject <sreject@comcast.net>
2019-11-23 11:38:41 -0500

    moved embedchat check into promise so mixer loads before resolve

--- src/injected-content/state/page.js
+++ src/injected-content/state/page.js
@@ -15,99 +15,98 @@ window.addEventListener('elixr:url-changed', function () {
 // state.page();
 function current() {
     if (!page) {
-        // remove query parameters and hash from uri
-        // remove /mobile suffix from uri
-        let uri = window.location.uri.replace(/[?#].*$/, '').replace(/\/mobile\/?$/, '');
+        page = urlDependentPromise(async resolve => {
+            await waitForMixer();
 
-        // Page: embedded chat
-        let isEmbedChat = embedChatMatch.exec(uri);
-        if (isEmbedChat != null) {
-            page = Promise.resolve({
-                type: 'embedded-chat',
-                channel: isEmbedChat[1]
-            });
+            // fullfilled due to url change
+            if (page.fullfilled) {
+                return;
+            }
 
-        // page deduction needs to wait for mixer to load
-        } else {
-            page = urlDependentPromise(async resolve => {
-                await waitForMixer();
+            // remove query parameters and hash from uri
+            // remove /mobile suffix from uri
+            let uri = window.location.uri.replace(/[?#].*$/, '').replace(/\/mobile\/?$/, '');
 
-                // fullfilled due to url change
-                if (page.fullfilled) {
-                    return;
-                }
-
-                let details = {
-                    desktop: $('b-channel-page-wrapper').length !== 0,
-                    mobile: $('b-channel-mobile-page-wrapper').length !== 0
-                };
-
-                // Page: home page
-                if ($('b-homepage').length !== 0) {
-                    return resolve({
-                        type: 'homepage'
-                    });
-                }
-
-                // Page: other/unknown
-                if (!details.desktop && !details.mobile) {
-                    return resolve({
-                        type: 'other'
-                    });
-                }
-
-                // Page: channel
-                details.type = 'channel';
-
-                // Attempt to get channel name from uri
-                let channelInUri = channelMatch.exec(uri);
-                if (channelInUri != null) {
-                    details.identifier = channelInUri[1];
-                    return resolve(details);
-                }
-
-                // Attempt to get channel name from dom
-                let pollPageForChannel = urlDependentPromise(resolve => {
-                    (function pollForChannelName() {
-                        if (pollPageForChannel.fullfilled) {
-                            return;
-                        }
-
-                        let channel = '';
-                        if (details.isDesktop) {
-                            channel = $('b-channel-profile')
-                                .find('h2')
-                                .first()
-                                .text();
-
-                        } else {
-                            channel = $('b-mobile-details-bar')
-                                .find('.name')
-                                .text();
-                        }
-                        channel = (channel || '').trim();
-
-                        if (channel === '') {
-                            setTimeout(pollForChannelName, 10);
-
-                        } else {
-                            log('Found channel name on page');
-                            resolve(channel);
-                        }
-                    }());
+            // Page: embedded chat
+            let isEmbedChat = embedChatMatch.exec(uri);
+            if (isEmbedChat != null) {
+                return resolve({
+                    type: 'embedded-chat',
+                    channel: isEmbedChat[1]
                 });
-                let identifier = await pollPageForChannel;
+            }
 
-                // promise fullfilled due to url change
-                if (!page || page.fullfilled) {
-                    return;
-                }
-                if (identifier != null) {
-                    details.identifier = identifier;
-                }
+
+            let details = {
+                desktop: $('b-channel-page-wrapper').length !== 0,
+                mobile: $('b-channel-mobile-page-wrapper').length !== 0
+            };
+
+            // Page: home page
+            if ($('b-homepage').length !== 0) {
+                return resolve({
+                    type: 'homepage'
+                });
+            }
+
+            // Page: other/unknown
+            if (!details.desktop && !details.mobile) {
+                return resolve({
+                    type: 'other'
+                });
+            }
+
+            // Page: channel
+            details.type = 'channel';
+
+            // Attempt to get channel name from uri
+            let channelInUri = channelMatch.exec(uri);
+            if (channelInUri != null) {
+                details.identifier = channelInUri[1];
                 return resolve(details);
+            }
+
+            // Attempt to get channel name from dom
+            let pollPageForChannel = urlDependentPromise(resolve => {
+                (function pollForChannelName() {
+                    if (pollPageForChannel.fullfilled) {
+                        return;
+                    }
+
+                    let channel = '';
+                    if (details.isDesktop) {
+                        channel = $('b-channel-profile')
+                            .find('h2')
+                            .first()
+                            .text();
+
+                    } else {
+                        channel = $('b-mobile-details-bar')
+                            .find('.name')
+                            .text();
+                    }
+                    channel = (channel || '').trim();
+
+                    if (channel === '') {
+                        setTimeout(pollForChannelName, 10);
+
+                    } else {
+                        log('Found channel name on page');
+                        resolve(channel);
+                    }
+                }());
             });
-        }
+            let identifier = await pollPageForChannel;
+
+            // promise fullfilled due to url change
+            if (!page || page.fullfilled) {
+                return;
+            }
+            if (identifier != null) {
+                details.identifier = identifier;
+            }
+            return resolve(details);
+        });
     }
     return page;
 }
