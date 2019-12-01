38d03afbada319f217bf377f71ef03fba24c19e2
SReject <sreject@comcast.net>
2019-11-30 23:17:43 -0500

    site wide script refactored

--- src/content-script-refactor/page/index.js
+++ src/content-script-refactor/page/index.js
@@ -1,5 +1,32 @@
-/*
-import page/area scripts here
-*/
+import state from '../state/';
 import './areas/';
 import './channel/';
+
+import { constellation } from '../mixer/';
+
+// start constellation connection for current user
+constellation.start();
+
+// when user updates, restart constellation
+window.addEventListener('MixrElixr:current-user:changed', function () {
+    constellation.stop();
+    constellation.start();
+});
+
+// listen for settings to change and apply theme
+window.addEventListener('MixrElixr:settings:changed', () => {
+    let theme = state.settings.cached().sitewide.theme;
+    if (theme == null || theme === 'default') {
+        document.body.removeAttribute('data-elixr-theme');
+    } else {
+        document.body.setAttribute('data-elixr-theme', theme);
+    }
+});
+
+// get settings, apply theme
+state.settings().then(settings => {
+    let theme = settings.sitewide.theme;
+    if (theme != null && theme !== 'default') {
+        document.body.setAttribute('data-elixr-theme', theme);
+    }
+});
