4f0ec8f4729342e938cd9833f69ec78d0bf930c7
SReject <sreject@comcast.net>
2019-12-12 05:29:20 -0500

    Finish moving theme handling

--- src/content-script-refactor/page/index.js
+++ src/content-script-refactor/page/index.js
@@ -1,4 +1,3 @@
-import state from '../state/';
 import './areas/';
 import './channel/';
 
@@ -12,24 +11,3 @@ window.addEventListener('MixrElixr:current-user:changed', function () {
     constellation.stop();
     constellation.start();
 });
-
-state
-    .settings()
-    .then(settings => {
-
-        // apply theme
-        let theme = settings.sitewide.theme;
-        if (theme != null && theme !== 'default') {
-            document.body.setAttribute('data-elixr-theme', theme);
-        }
-
-        // listen for settings to change and apply theme
-        window.addEventListener('MixrElixr:settings:changed', () => {
-            let theme = state.settings.cached().sitewide.theme;
-            if (theme == null || theme === 'default') {
-                document.body.removeAttribute('data-elixr-theme');
-            } else {
-                document.body.setAttribute('data-elixr-theme', theme);
-            }
-        });
-    });
