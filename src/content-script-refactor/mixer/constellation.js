2b498efe8e643fc8ee4a5e5f78be886c4cbc114c
SReject <sreject@comcast.net>
2019-12-13 14:36:24 -0500

    Updated to be less reliant on state. Listens for events to know when to load and when to (un)sub to user slugs

--- src/content-script-refactor/mixer/constellation.js
+++ src/content-script-refactor/mixer/constellation.js
@@ -3,23 +3,61 @@ import { Carina } from 'carina';
 import state from '../state/';
 import { emit } from '../utils/';
 
-let ca;
+let constellation,
+    slugUserId;
 
-export function stop() {
-    if (ca != null) {
-        ca.close();
-        ca = null;
+function onUpdate(data) {
+    let user = state.user.cached(),
+        userId = user ? user.id : null;
+
+    // Current user doesn't match the user being monitored
+    if (userId !== slugUserId) {
+        switchUser(userId); // eslint-disable-line no-use-before-define
+
+    // Emit Event: MixrElixr:constellation:user-update
+    } else {
+        emit('constellation:user-update', data);
     }
 }
 
-export async function start() {
-    stop();
+function switchUser(userId) {
 
-    let user = await state.user();
-    if (!user) return;
+    // no need to switch
+    if ((userId == null && slugUserId == null) || userId === slugUserId) {
+        return;
+    }
 
-    ca = new Carina({ isBot: true }).open();
-    ca.subscribe(`user:${user.id}:update`, data => {
-        emit('current-user:update', { detail: data });
-    });
+    // Unsubscribe from previous slug
+    if (userId != null) {
+        constellation.unsubsubscribe(`user:${userId}:update`);
+    }
+
+    // Subscribe to new slug
+    if (userId != null) {
+        slugUserId = userId;
+        constellation.subscribe(`user:${slugUserId}:update`, onUpdate);
+    }
 }
+
+// Wait for mixer to load
+window.addEventListener('MixrElixr:load:init', evt => {
+
+    // start constellation
+    constellation = new Carina({ isBot: true }).open();
+
+    // if the user is logged in, subscribe to the user update slug
+    let user = evt.details.user;
+    if (user && user.id) {
+        switchUser(user.id);
+    }
+});
+
+// User logged in: switch user slug
+window.addEventListener('MixrElixr:user:login', evt => {
+    switchUser(evt.details.user.id);
+});
+
+// User logged out: unsub from user slug
+window.addEventListener('MixrElixr:user:logout', () => {
+    switchUser();
+});
